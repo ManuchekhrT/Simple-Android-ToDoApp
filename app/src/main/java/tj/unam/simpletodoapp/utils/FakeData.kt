50a01c35c7f88f6e3bfe87204e4350af375b95ea
ManuchekhrT <manu_tera@mail.ru>
2019-04-12 11:24:06 +0500

    * changed constants fake data

--- app/src/main/java/tj/unam/simpletodoapp/utils/FakeData.kt
+++ app/src/main/java/tj/unam/simpletodoapp/utils/FakeData.kt
@@ -5,9 +5,9 @@ import java.util.*
 
 object FakeData {
 
-    private const val FAKE_DATA_1 = "A simple note"
-    private const val FAKE_DATA_2 = "A note with a line feed"
-    private const val FAKE_DATA_3 = "It is a pleasure to meet such a talented man like u"
+    private const val FAKE_DATA_1 = "First simple note"
+    private const val FAKE_DATA_2 = "Second simple note"
+    private const val FAKE_DATA_3 = "Third simple note"
 
     private fun getDate(diff: Int): Date {
         val calendar = GregorianCalendar()
